b5518df1c6fdce9b29441538198e2ac4703af5c9
LRaev <LRaev@DESKTOP-BK6F79D>
2022-01-24 19:42:16 +0200

    Client init

--- Core/Program.cs
+++ Core/Program.cs
@@ -40,7 +40,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
      {
-         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+         c.SwaggerEndpoint("/swagger/v1/swagger.json", "File Upload  API V1");
      });
 }
 
